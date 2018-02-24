78e77b2550a27e2f5dfdcd53af8dfe800438ccdf
Michael Gregoire <mdgregoire@gmail.com>
2018-02-24 13:01:47 -0600

    images stored on db, writes to dom on page load, on click of image function is called with id passed to it

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,10 +6,45 @@ let self = this;
 self.imageArray = [];
 self.imageObject = {};
 
+////The code below was used for testing////
+// function Image(url, text, voteCount){
+//   this.url = url;
+//   this.note = note;
+//   this.voteCount = voteCount;
+// }
+// //end Image Constructor
+//
+// self.image1 = new Image('../assets/1.jpg', '111111')
+// console.log(self.image1, 'image1');
+// self.imageArray.push(self.image1);
+// console.log(self.imageArray, 'imageArray');
+// self.image1 = new Image('../assets/2.png', '222222')
+// console.log(self.image1, 'image1');
+// self.imageArray.push(self.image1);
+// console.log(self.imageArray, 'imageArray');
+
+self.getImages = function(){
+console.log('inget images');
+  $http({
+    method: 'GET',
+    url: '/gallery'
+  })
+  .then(function(response){
+    console.log('success in getImages', response.data);
+    self.imageArray = response.data;
+    console.log(self.imageArray);
+  })
+  .catch(function(error){
+    console.log('error in getImages', error);
+  })
+
+}
+//end getImages
+self.getImages();
 
 
-self.flipImage = function(){
-  console.log('in flip');
+self.flipImage = function(id){
+  console.log('in flip', id);
 }
 
 
--- server/routers/galleryRouter.js
+++ server/routers/galleryRouter.js
@@ -2,7 +2,19 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
-
+router.get('/', (request, response) => {
+  console.log('inside the get');
+  const sqlText = `SELECT * from images`
+  pool.query(sqlText)
+    .then((result) => {
+      response.send(result.rows);
+      console.log('success in the get router', result.rows);
+    })
+    .catch((error) =>{
+      console.log('error in get', error);
+      response.sendStatus(500);
+    })
+})//end the get
 
 
 
