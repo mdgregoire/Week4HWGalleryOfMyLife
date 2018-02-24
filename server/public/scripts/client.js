dd1f7f7e889abdcf810017e07ae523a3ebec4392
Michael Gregoire <mdgregoire@gmail.com>
2018-02-24 12:01:41 -0600

    js is working

---
+++ server/public/scripts/client.js
@@ -0,0 +1,9 @@
+const app = angular.module('myApp',[])
+
+
+const GalleryController = app.controller('GalleryController', ['$http', function($http){
+
+console.log('js');
+
+
+}]);//end GalleryController
