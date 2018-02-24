a0e0453ecabdb253a68fedf28ca5c027bcb7a592
Michael Gregoire <mdgregoire@gmail.com>
2018-02-24 15:19:34 -0600

    image now toggles between text and picture

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,24 +4,6 @@ const app = angular.module('myApp',[])
 const GalleryController = app.controller('GalleryController', ['$http', function($http){
 let self = this;
 self.imageArray = [];
-self.imageObject = {};
-
-////The code below was used for testing////
-// function Image(url, text, voteCount){
-//   this.url = url;
-//   this.note = note;
-//   this.voteCount = voteCount;
-// }
-// //end Image Constructor
-//
-// self.image1 = new Image('../assets/1.jpg', '111111')
-// console.log(self.image1, 'image1');
-// self.imageArray.push(self.image1);
-// console.log(self.imageArray, 'imageArray');
-// self.image1 = new Image('../assets/2.png', '222222')
-// console.log(self.image1, 'image1');
-// self.imageArray.push(self.image1);
-// console.log(self.imageArray, 'imageArray');
 
 self.getImages = function(){
 console.log('inget images');
@@ -43,8 +25,21 @@ console.log('inget images');
 self.getImages();
 
 
-self.flipImage = function(id){
-  console.log('in flip', id);
+self.flipImage = function(id, is_clicked){
+  console.log('in flip', id, is_clicked);
+  $http({
+    method: 'PUT',
+    url: `/gallery/${id}`,
+    data: {clicked: is_clicked}
+  })
+  .then(function(response){
+    console.log('success in putclick', response);
+    self.getImages();
+
+  })
+  .catch(function(error){
+    console.log('error in getImages', error);
+  })
 }
 
 
--- server/routers/galleryRouter.js
+++ server/routers/galleryRouter.js
@@ -4,7 +4,8 @@ const pool = require('../modules/pool');
 
 router.get('/', (request, response) => {
   console.log('inside the get');
-  const sqlText = `SELECT * from images`
+  const sqlText = `SELECT * from images
+                    ORDER BY id`
   pool.query(sqlText)
     .then((result) => {
       response.send(result.rows);
@@ -16,6 +17,31 @@ router.get('/', (request, response) => {
     })
 })//end the get
 
+router.put('/:id', (request, response) => {
+  console.log('inside the put');
+  let id = request.params.id;
+  let clicked;
+  if (request.body.clicked){
+    clicked = false;
+  }
+  else{
+    clicked = true;
+  }
 
+  console.log('in put', id, clicked);
+  const sqlText = `UPDATE images
+                   SET is_clicked = $1
+                   WHERE id = $2;`;
+  console.log('sqlText in put', sqlText);
+  pool.query(sqlText, [clicked, id])
+    .then((result) => {
+      response.sendStatus(200);
+      console.log('success in the put router', result);
+    })
+    .catch((error) =>{
+      console.log('error in put', error);
+      response.sendStatus(500);
+    })
+})//end the get
 
 module.exports = router;
