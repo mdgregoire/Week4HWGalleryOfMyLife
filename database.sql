9239dd01c75742fe270d290c90ee2e3510bc4040
Michael Gregoire <mdgregoire@gmail.com>
2018-02-25 22:02:19 -0600

    updated db and css

--- database.sql
+++ database.sql
@@ -30,9 +30,9 @@ create table comment (
 
 
 INSERT INTO images (url, note, title)
-VALUES ('https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/10402653_242062455990570_2378029888314616608_n.jpg?oh=17621670e769b6bf8a9f0d93f349ce60&oe=5B196D32', '11111', 'Young Mike'),
-('https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/227338_200119796692295_4952406_n.jpg?oh=4f1ccf97464afcc7d33e7819a4857041&oe=5B4DA811', '22222', 'Tending the Burn'),
-('https://www.ely.org/_site_components/uploads/fall/fall%20lake.jpg', '33333', 'Our Lake'),
-('https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/556851_10200162722699612_1548759812_n.jpg?oh=2f442a431448d06e9f3831eb3fbb242d&oe=5B452B58', '44444', 'Nappa Valley'),
-('https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/60156_1625610888975_4196436_n.jpg?oh=40fc92a37522696d61044fc7af9c11d0&oe=5B04B0B3', '55555', 'Walking Away'),
-('https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/61339_527643456761_7745328_n.jpg?oh=167badd96330fd88ff1ab3d35f4a5c11&oe=5B1BB27E', '66666', 'Catering & Camping');
+VALUES ('https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/10402653_242062455990570_2378029888314616608_n.jpg?oh=17621670e769b6bf8a9f0d93f349ce60&oe=5B196D32', 'Just a picture of me when I was young.  ', 'Young Mike'),
+('https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/227338_200119796692295_4952406_n.jpg?oh=4f1ccf97464afcc7d33e7819a4857041&oe=5B4DA811', 'Every winter I go up north with my family to our cabin and we burn the brush from the summer. ', 'Tending the Burn'),
+('https://www.ely.org/_site_components/uploads/fall/fall%20lake.jpg', 'Our lake.  It is my favorite place to be.', 'Our Lake'),
+('https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/556851_10200162722699612_1548759812_n.jpg?oh=2f442a431448d06e9f3831eb3fbb242d&oe=5B452B58', 'Spent the entire trip tasting wine and eating too much food, sums up most of my vacations.', 'Nappa Valley'),
+('https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/60156_1625610888975_4196436_n.jpg?oh=40fc92a37522696d61044fc7af9c11d0&oe=5B04B0B3', 'Starting a hike.  Being out in the woods is a great way for me to recharge.', 'Woods'),
+('https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/61339_527643456761_7745328_n.jpg?oh=167badd96330fd88ff1ab3d35f4a5c11&oe=5B1BB27E', '1 week of camping led up to a meal for 300 cooked in the open with nothing that came from more than 10 miles away.', 'Catering & Camping');
